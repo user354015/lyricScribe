bca7737b8783c8214405d72538d0a70888b9d421
user354015 <g354015@protonmail.com>
2025-08-12 14:21:43 +0300

    Forgot to git add config.go

---
+++ config.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/BurntSushi/toml"
+)
+
+const (
+	PlayerPrefix string = "org.mpris.MediaPlayer2."
+	PlayerPath   string = "/org/mpris/MediaPlayer2"
+
+	FetchApiUrl    string = "https://lrclib.net/api/get?"
+	FetchSearchUrl string = "https://lrclib.net/api/search?q="
+)
+
+type ConfigOptions struct {
+	General struct {
+		ProgramName string `toml:"program_name"`
+		ProgramMode string `toml:"program_mode"`
+		Logging     string `toml:"logging"`
+	} `toml:"general"`
+
+	Internal struct {
+		ApiUrl    string `toml:"api_url"`
+		SearchUrl string `toml:"search_url"`
+	} `toml:"internal"`
+
+	Search struct {
+		Depth string `toml:"depth"`
+	} `toml:"search"`
+
+	Player struct {
+		Player         string  `toml:"player"`
+		PositionOffset float64 `toml:"position_offset"`
+		Step           float64 `toml:"step"`
+		SilenceTimeout int     `toml:"silence_timeout"`
+	} `toml:"player"`
+}
+
+var c ConfigOptions
+
+func ReadConfig() ConfigOptions {
+	configPath, e := os.UserConfigDir()
+	Check(e)
+	configPath = filepath.Join(configPath, "lyrics", "config.toml")
+
+	config := &ConfigOptions{}
+
+	_, e = toml.DecodeFile(configPath, config)
+	Check(e)
+
+	c = *config
+	return *config
+}
